608654d17daf4196d88cd34ea10a06f0f5dc6a0b
Raf-Taufiqurrahman <raf.taufiqurrahman@gmail.com>
2022-06-21 22:11:26 +0800

    update restapi product

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProductController;
 
 Route::controller(ProductController::class)->group(function(){
-    Route::get('/product', 'index')->name('index');
-    Route::get('/product/{slug}', 'show')->name('show');
+    Route::get('/product', 'index');
+    Route::get('/landing/product', 'landing');
+    Route::get('/product/{slug}', 'show');
 });
